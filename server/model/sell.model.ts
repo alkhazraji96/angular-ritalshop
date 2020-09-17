1a3f9dd9acad169d0362ec6c0f2c0a2586f17037
Ameer Alkhazraji <ameer.alkhazraji96.com>
2020-09-17 15:11:06 +0300

    empty

--- server/model/sell.model.ts
+++ server/model/sell.model.ts
@@ -2,7 +2,6 @@ import { Schema, model } from 'mongoose'
 
 const SellSchema = new Schema({
   type: { type: String, required: true },
-  receipt: { type: Schema.Types.ObjectId, ref: 'Receipt' },
   qty: { type: Number },
   price: { type: Number },
   createdAt: { type: Date, default: Date.now }
